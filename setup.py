f5ca507a42406f8410ea63d2ee11bffbcea31171
Maxence Buisson <maxence.buisson2@orange.fr>
2022-07-27 15:55:06 +0200

    numpy auto

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 #!/usr/bin/python3
 from distutils.core import setup
 from distutils.core import Extension
-
+import numpy
+incdir = numpy.get_include()
 Sigma=Extension("sigmaV1",["src/Sigma.c","src/algorithm_2.c","src/algorithm_3.c","src/matrix_0_1.c","src/partition_tree.c","src/files.c","src/graph.c"],
                     define_macros = [('MAJOR_VERSION', '1'),
                                     ('MINOR_VERSION', '0')],
-                    include_dirs=['include','/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/numpy/core/include'])
+                    include_dirs=['include',incdir])
 setup(
     name='SiGma',
    version='1.0',
